57ba2177d838845a86a312375290647870293032
JC <juancarlosls200410@gmail.com>
2023-05-12 11:39:03 +0200

    pasamos el proyecto completo a angular

--- Angular/angular_ejercicio2/src/app/app.module.ts
+++ Angular/angular_ejercicio2/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { LogingComponent } from './loging/loging.component';
 import { PieDePaginaComponent } from './pie-de-pagina/pie-de-pagina.component';
 import { UsuarioComponent } from './usuario/usuario.component';
@@ -12,7 +11,6 @@ import { BuscadorComponent } from './buscador/buscador.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     LogingComponent,
     PieDePaginaComponent,
     UsuarioComponent,
