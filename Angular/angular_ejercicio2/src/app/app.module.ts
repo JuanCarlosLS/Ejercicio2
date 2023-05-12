e5404bc2bcf31862b4654ca55af943011964c8a9
JC <juancarlosls200410@gmail.com>
2023-05-12 09:54:13 +0200

    creamos loging angular

--- Angular/angular_ejercicio2/src/app/app.module.ts
+++ Angular/angular_ejercicio2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { LogingComponent } from './loging/loging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LogingComponent
   ],
   imports: [
     BrowserModule,
